8ea86eeb5cd8587718ce7988674db17e744e62f2
Nathan Pawloski <natepawloski@gmail.com>
2017-10-27 10:09:41 -0400

    changed details page

---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        [MinLength(2), MaxLength(50)]
+        public string title { get; set; }
+        [Display(Name ="Published Date")]
+        public DateTime PublishedDate { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+        //[Required]
+        public string ISBN { get; set; }
+
+        [ForeignKey("Category")]
+        public int Cetagory { get; set; }
+        public virtual Category Category { get; set; }
+
+        
+    }
+}
